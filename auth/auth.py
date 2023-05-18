100e89f2eb506442b523d9d80329f1e1c8121e36
canltn <lethingoccanst@gmail.com>
2023-05-18 19:33:11 +0700

    edit domain

--- auth/auth.py
+++ auth/auth.py
@@ -5,9 +5,9 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-AUTH0_DOMAIN = 'ry-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-znufqirkws7isg6q.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'fsnd-capstone'
+API_AUDIENCE = 'http://localhost:5000'
 
 
 # AuthError Exception
